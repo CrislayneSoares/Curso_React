d8bb11de0ff00c46eacf207e8f9bc5f4453dc3a3
Crislayne Soares <111311061+CrislayneSoares@users.noreply.github.com>
2023-06-05 21:46:18 -0300

    Add data

--- projeto1/src/components/Quiz.jsx
+++ projeto1/src/components/Quiz.jsx
@@ -3,24 +3,11 @@ import '../components/quiz.css'
 
 function Quiz(urlImgQuiz){
 
-    const pergunatas = ["Em que ano o BTS debutou?", "Qual o nome do fandom do BTS?" , 3 , 4, 5]
-    const respostas = ["13/06/2013", "Army"]
-    const alternativas = ["15/05/2014", "13/06/2013", "12/03/2012", "Bell", "Bomb", "Army"]
-
     const [pergunta, setPergunta] = useState(pergunatas[0])
     const [escolha, setEscolha] = useState()
     
     function clicou(){
-        
-        if (alternativas[0] == respostas[0]) {
-            setPergunta(pergunatas[1])
-        }
-        if (alternativas[1] == respostas[0]) {
-            setPergunta(pergunatas[1])
-        }
-        if (alternativas[2] == respostas[0]) {
-            setPergunta(pergunatas[1])
-        }
+        pass
     }
 
     return (
---
+++ projeto1/src/data/question.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+      question: "Em que ano o BTS debutou?",
+      options: [
+        "13/04/2015",
+        "13/06/2013",
+        "12/06/2013",
+        "13/03/2013",
+      ],
+      answer: "13/06/2013",
+    },
+    {
+      question: "Em qual cidade o Jimin nasceu?",
+      options: ["Busan", "Geodoen", "Seul", "Ilsan"],
+      answer: "Busan",
+    },
+    {
+      question: "Qual é o membro mais novo do grupo?",
+      options: ["Jimin", "J Hope", "Jungkook", "V"],
+      answer: "Jungkook",
+    },
+    {
+      question: "Qual dos menbros é o mais velho?",
+      options: ["J Hope", "Suga", "NamJoom", "Jin"],
+      answer: "Jin",
+    },
+    {
+      question:
+        "Qual o nome do fandom do BTS?",
+      options: ["Bell", "Army", "Bomb", "Proof"],
+      answer: "Army",
+    },
+  ];
+  
+  export default data;
